cb75a53ecc7d25226227d5b39ea60742066a7ee3
suint <lui.ysia@gmail.com>
2018-04-26 02:11:49 -0400

    no more card class

--- lib/src/main/java/com/example/lib/Board.java
+++ lib/src/main/java/com/example/lib/Board.java
@@ -33,8 +33,8 @@ public class Board {
     gamePlayers.add(p);
   }
   
-  public void addTile(Tile t){
-    gameTiles[0] = t; //would add tile onto end of list of tiles... not an arraylist since num of tiles is fixed
+  public void addTile(Tile t, int x){
+    gameTiles[x] = t; //would add tile onto end of list of tiles... not an arraylist since num of tiles is fixed
   }
   
   public Player getPlayer(int j){
--- lib/src/main/java/com/example/lib/CardTile.java
+++ lib/src/main/java/com/example/lib/CardTile.java
@@ -5,25 +5,33 @@ package com.example.lib;
  */
 
 public class CardTile extends Tile {
-  public Card[] cardlist;
+  public enum CardStack {
+    COMMUNITYCHEST, CHANCE
+  } //which type of stack of cards
+  
+  public String[] cardlist;
+  public CardStack type;
   
   //when created generates random list of cards
   
-  public CardTile(String name, int pos, String imgFile) {
+  public CardTile(String name, int pos, String imgFile, CardStack c) {
     super(name, pos, imgFile);
+    this.type = c;
     this.cardlist = makeCards();
   }
   
-  private Card[] makeCards(){
-    Card[] cards = new Card[10];
-    for (int i = 0; i < 10; i++){
-      cards[i] = new Card();
+  private String[] makeCards(){
+    String[] cards = new String[16];
+    if (type == CardStack.CHANCE){
+      cards[1] = "";
     }
+    
     return cards;
   }
   
   //randomly draws a card, and updates the user based on the card drawn
-  public void drawCard(int x) {
-  
+  public String drawCard(int x) {
+    String a = cardlist[x];
+    return a;
   }
 }
